ebda31e14aad15e72caf18a1c191e4fcd09c4087
Balazs Varga <b4zs@b4zs.com>
2015-08-07 22:31:15 +0200

    initial

---
+++ NamespacedMemcache.php
@@ -0,0 +1,84 @@
+<?php
+
+
+class NamespacedMemcache extends Memcache
+{
+	const NAMESPACE_PREFIX = 'tescomobile';
+
+	private static $instance;
+
+	private $namespaceVersion = 0;
+
+	private $alreadyConnected = false;
+
+	public static function getInstance()
+	{
+		if (null === self::$instance) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	public function connect($host, $port, $timeout = 1)
+	{
+		if (!$this->alreadyConnected) {
+			parent::connect($host, $port, $timeout);
+			$this->initializeKeyNamespace();
+
+			$this->alreadyConnected = true;
+		}
+	}
+
+	public function flush()
+	{
+		$this->incrementNamespaceVersion();
+
+		return true;
+	}
+
+	public function add($key, $var, $flag, $expire)
+	{
+		return parent::add($this->prependNamespace($key), $var, $flag, $expire);
+	}
+
+	public function get($key, &$flags = 0)
+	{
+		return parent::get($this->prependNamespace($key), $flags);
+	}
+
+	public function set($key, $var, $flag, $expire)
+	{
+		return parent::set($this->prependNamespace($key), $var, $flag, $expire);
+	}
+
+	public function delete($key, $timeout = 0)
+	{
+		return parent::delete($this->prependNamespace($key), $timeout);
+	}
+
+	private function initializeKeyNamespace()
+	{
+		$this->namespaceVersion = parent::get($this->getNamespacePrefix());
+
+		if (!$this->namespaceVersion) {
+			$this->namespaceVersion = 0;
+			$this->incrementNamespaceVersion();
+		}
+	}
+
+	private function incrementNamespaceVersion()
+	{
+		parent::set($this->getNamespacePrefix(), ++$this->namespaceVersion);
+	}
+
+	private function prependNamespace($key)
+	{
+		return $this->getNamespacePrefix() . '_' . $this->namespaceVersion . '_'.$key;
+	}
+
+	private function getNamespacePrefix()
+	{
+		return $_SERVER['HTTP_HOST'];
+	}
+}
